5849fe21e81192822e49b75fbf444003ca7b09aa
Michael Ashton <mykl.ashton@gmail.com>
2017-07-31 10:56:50 -0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+          <h1>You're good to go! Now fix the internet...</h1>
+      </div>
+    );
+  }
+}
