784539b13919efbb704555f43dd7ca06b28bcb38
Michael Ashton <mykl.ashton@gmail.com>
2017-08-01 13:37:02 -0400

    Fix more

--- src/TempHeader.js
+++ src/TempHeader.js
@@ -1,13 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-// export default class TempHeader extends Component {
-//     render() {
-//         return (
-//
-//         )
-//     }
-// }
 const TempHeader = (props) => {
     return (
         <div>
