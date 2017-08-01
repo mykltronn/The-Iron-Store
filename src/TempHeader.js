d947ae1990a2032b9bb324b40791519245b4931e
Michael Ashton <mykl.ashton@gmail.com>
2017-08-01 13:35:26 -0400

    Fix routing, add {props.children} to TempHeader

--- src/TempHeader.js
+++ src/TempHeader.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+// export default class TempHeader extends Component {
+//     render() {
+//         return (
+//
+//         )
+//     }
+// }
 const TempHeader = (props) => {
     return (
-        <nav>
-            <NavLink activeClassName="selected" className="nav-link" exact to="/">Splash</NavLink>
-            <NavLink activeClassName="selected" className="nav-link" exact to="/shop">Shop</NavLink>
-            <NavLink activeClassName="selected" className="nav-link" exact to="/checkout">Checkout</NavLink>
-            <NavLink activeClassName="selected" className="nav-link" exact to="/reviews">Reviews</NavLink>
-            <NavLink activeClassName="selected" className="nav-link" exact to="/admin">Admin</NavLink>
-        </nav>
+        <div>
+            <nav>
+                <NavLink activeClassName="selected" className="nav-link" exact to="/">Splash</NavLink>
+                <NavLink activeClassName="selected" className="nav-link" exact to="/shop">Shop</NavLink>
+                <NavLink activeClassName="selected" className="nav-link" exact to="/checkout">Checkout</NavLink>
+                <NavLink activeClassName="selected" className="nav-link" exact to="/reviews">Reviews</NavLink>
+                <NavLink activeClassName="selected" className="nav-link" exact to="/admin">Admin</NavLink>
+            </nav>
+            {props.children}
+        </div>
     )
 }
 
