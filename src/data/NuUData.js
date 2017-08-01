1e5ddc44da46d524e9fef5e86207f0c84c0bfc5b
Robert Hutchins <rjhutchins22@gmail.com>
2017-08-01 13:55:39 -0400

    Add some temp data to NuUData

---
+++ src/data/NuUData.js
@@ -0,0 +1,17 @@
+var NuUData =
+{
+  results : [
+  {
+    title: "Lumber Jack",
+    description: "Rather than be clean-shaven, the Lumbersexual has an unkempt beard. The Metrosexual is clean and pretty and well-groomed; the Lumbersexual spends the same amount of money, but looks filthy. Sartorially speaking, a Lumbersexual is a delicate tri-blend of L.L. Bean, Timberlake, and Sears.",
+    image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiZipipybbVAhURayYKHWCzAlYQjRwIBw&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dp9hluLlBBsk&psig=AFQjCNFnjSh9Z13mZ0Oo-veCtYkd9T2MdQ&ust=1501695363916496"
+  },
+  {
+    title: "Hipster",
+    description: "Hipsters are a subculture of men and women typically in their 20's and 30's that value independent thinking, counter-culture, progressive politics, an appreciation of art and indie-rock, creativity, intelligence, and witty banter. The greatest concentrations of hipsters can be found living in the Williamsburg, Wicker Park, and Mission District neighborhoods of major cosmopolitan centers such as New York, Chicago, and San Francisco respectively",
+    image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiWz76uyrbVAhVH6yYKHXAYAtQQjRwIBw&url=https%3A%2F%2Fwww.theodysseyonline.com%2Fwhat-actually-hipster&psig=AFQjCNG_U5UZEClHifNVIYgTRE-bBJMuzg&ust=1501695604132203"
+  }
+  ]
+}
+
+export NuUData;
