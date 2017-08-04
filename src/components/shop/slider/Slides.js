43a317d7928bcbdbe11abc1ebb1854f89fe5c217
Michael Ashton <mykl.ashton@gmail.com>
2017-08-04 13:12:44 -0400

    temp update

--- src/components/shop/slider/Slides.js
+++ src/components/shop/slider/Slides.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import Slide from './Slide.js'
 
 export default class Slides extends Component {
-    // slides will need to fetch from the options list endpoint, chage line 8
     render() {
         let current = this.props.slide
         let slidesNodes = this.props.data.map((slideNode, index) => {
             let isActive = current == index;
             return (
-                <Slide getOption={this.props.getOptions} active={isActive} key={slideNode.id} ident={slideNode.id} label={slideNode.label} options={slideNode.options} />
+                <Slide getOption={this.props.getOptions} active={isActive} key={slideNode.id} title={slideNode.title} label={slideNode.label} options={slideNode.options} />
                 )
             })
 
