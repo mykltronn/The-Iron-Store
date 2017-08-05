ac550168612ace768ad9c0a56348512401f88047
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-08-05 16:00:33 -0400

    logo changed

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -16,7 +16,7 @@ class Splash extends Component {
         return (
             <div className="bodydiv">
               <div className="titlediv">
-                <img className="logo" alt="logo" src={"logo2.png"}/>
+                <img className="logo" alt="logo" src={"NuU.png"}/>
                 <p>The New You is Ready</p>
               </div>
               <div className="buttondiv">
