029897b0ec44188067d345fff08a779ba1f4b67a
zoe-c <find.zoec@gmail.com>
2017-08-07 15:23:35 -0400

    hidden header, quick tab fix. can finesse later. NOTE: does not use med. queries by ethan. can be adjusted to each, is not currently. styles are now in a sep css file than index.css

--- src/components/TempHeader/TempHeader.js
+++ src/components/TempHeader/TempHeader.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './TempHeader.css'
+
 const TempHeader = (props) => {
     return (
         <div>
@@ -10,6 +12,7 @@ const TempHeader = (props) => {
                 <NavLink activeClassName="selected" className="nav-link" exact to="/reviews">Reviews</NavLink>
                 <NavLink activeClassName="selected" className="nav-link" exact to="/admin">Admin</NavLink>
             </nav>
+            <div className= "nav-indicator-tab"> Menu ▲ </div>
             {props.children}
         </div>
     )
