55e947c75974f8a6ad22a39fab3ecb1f8c4fe8b3
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-08-07 08:54:12 -0400

    removed checkout from nav bar

--- src/components/TempHeader/TempHeader.js
+++ src/components/TempHeader/TempHeader.js
@@ -7,7 +7,6 @@ const TempHeader = (props) => {
             <nav>
                 <NavLink activeClassName="selected" className="nav-link" exact to="/">Home</NavLink>
                 <NavLink activeClassName="selected" className="nav-link" exact to="/shop">Shop</NavLink>
-                <NavLink activeClassName="selected" className="nav-link" exact to="/checkout">Checkout</NavLink>
                 <NavLink activeClassName="selected" className="nav-link" exact to="/reviews">Reviews</NavLink>
                 <NavLink activeClassName="selected" className="nav-link" exact to="/admin">Admin</NavLink>
             </nav>
