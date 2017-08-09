4d99c2ddf8f9ae2e24556a711ae5209d4ddc0ca0
zoe-c <find.zoec@gmail.com>
2017-08-09 10:40:05 -0400

    syncing branch with dev

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -55,10 +55,8 @@ export default class Shop extends Component {
         return (
         <div className="shop-body-wrapper move-to">
             <TempHeader />
-
-
-            <div className="shop-title-desc">revitalizing your online presence begins with deciding who you want to be.  You can be anyone you want to be.  Now is your chance to re-create yourself.</div>
-            <div className="shop-title">Make your selection from these options</div>
+            <h4 className="shop-title-desc">Revitalizing your online presence begins with deciding who you want to be.  You can be anyone you want to be.  Now is your chance to re-create yourself.</h4>
+            <h1 className="shop-title" > Make your selection from these options </h1>
             <div className="shop-component">
                 {productCard}
             </div>
